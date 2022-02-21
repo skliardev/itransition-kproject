729d6cd844252a405dd308caeccf11722aee682c
skliardev <skliardev@gmail.com>
2022-02-21 09:41:11 +0300

    add service.config / appsetting write company information / load config section about company

---
+++ Service/Config.cs
@@ -0,0 +1,15 @@
+
+namespace project.Service;
+
+public class Config
+{
+    public static string CompanyName { get; set; }
+
+    public static string CompanyEmail { get; set; }
+
+    public static string CompanyPhoneFull { get; set; }
+
+    public static string CompanyPhoneShort { get; set; }
+
+    public static string CompanyDesigner { get; set; }
+}
